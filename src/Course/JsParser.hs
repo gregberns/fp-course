bd32f29bbc5af526bfdb4338b444bcf2cb2e37a9
Greg Berns <gregberns@gmail.com>
2018-08-15 14:52:18 -0700

    if expressions basically working
    bug in expressions though

--- src/Course/JsParser.hs
+++ src/Course/JsParser.hs
@@ -16,6 +16,11 @@ import Course.Monad
 import Course.List
 import Course.Optional
 
+data JsBoolean =
+  JsTrue
+  | JsFalse
+  deriving (Eq, Ord, Show)
+
 data JsBinOp =
   JsBinEq
   | JsBinNotEq
@@ -33,10 +38,15 @@ data JsUnaryOp =
   JsUnaryOpNot
   deriving (Eq, Ord, Show)
 
-data JsBoolean =
-  JsTrue
-  | JsFalse
+data JsExpr =
+  JsBoolExpr JsBoolean
+  | JsParenExpr JsExpr
+  | JsUnaryExpr (JsUnaryOp, JsExpr)
+  | JsBinExpr (JsExpr, JsBinOp, JsExpr)
   deriving (Eq, Ord, Show)
+
+jsCommaTok :: Parser Chars
+jsCommaTok = stringTok ";"
 
 jsTrue :: Parser Chars
 jsTrue = stringTok "true"
@@ -45,19 +55,39 @@ jsFalse :: Parser Chars
 jsFalse = stringTok "false"
 
 jsBoolean :: Parser JsBoolean
-jsBoolean =
-  JsTrue <$ jsTrue ||| JsFalse <$ jsFalse
+jsBoolean = JsTrue <$ jsTrue ||| JsFalse <$ jsFalse
 
+jsBinEqual :: Parser Chars
 jsBinEqual = stringTok "=="
+
+jsBinNotEqual :: Parser Chars
 jsBinNotEqual = stringTok "!="
+
+jsBinStrictEqual :: Parser Chars
 jsBinStrictEqual = stringTok "==="
+
+jsBinStrictNotEqual :: Parser Chars
 jsBinStrictNotEqual = stringTok "!=="
+
+jsBinGreaterThan :: Parser Chars
 jsBinGreaterThan = stringTok ">"
+
+jsBinGreaterThanOrEqual :: Parser Chars
 jsBinGreaterThanOrEqual = stringTok ">="
+
+jsBinLessThan :: Parser Chars
 jsBinLessThan = stringTok "<"
+
+jsBinLessThanOrEqual :: Parser Chars
 jsBinLessThanOrEqual = stringTok "<="
+
+jsBinAnd :: Parser Chars
 jsBinAnd = stringTok "&&"
+
+jsBinOr :: Parser Chars
 jsBinOr = stringTok "||"
+
+jsBinNot :: Parser Chars 
 jsBinNot = stringTok "!"
 
 jsBinOperator :: Parser JsBinOp
@@ -81,12 +111,6 @@ jsUnaryOp =
     JsUnaryOpNot <$ jsBinNot
   )
 
-data JsExpr =
-  JsBoolExpr JsBoolean
-  | JsParenExpr JsExpr
-  | JsUnaryExpr (JsUnaryOp, JsExpr)
-  | JsBinExpr (JsExpr, JsBinOp, JsExpr)
-  deriving (Eq, Ord, Show)
 
 -- parse jsUnaryExpr "!true"
 -- Result >< JsUnaryExp LogicalAnd (Bool JsTrue) (Bool JsTrue)
@@ -111,11 +135,62 @@ jsBinExpr =
     b2 <- jsExpr
     pure (b1, o, b2)
 
+-- parse jsExpr "true !== false"
 jsExpr :: Parser JsExpr
 jsExpr =
   (
+    JsBinExpr <$> jsBinExpr
+    |||
     JsBoolExpr <$> jsBoolean
-    ||| JsBinExpr <$> jsBinExpr
-    ||| JsUnaryExpr <$> jsUnaryExpr
-    
+    ||| 
+    JsUnaryExpr <$> jsUnaryExpr
+  )
+
+jsIfTok :: Parser Chars
+jsIfTok = stringTok "if"
+
+jsLtParen :: Parser Chars
+jsLtParen = stringTok "("
+
+jsRtParen :: Parser Chars
+jsRtParen = stringTok ")"
+
+jsLtBrace :: Parser Chars
+jsLtBrace = stringTok "{"
+
+jsRtBrace :: Parser Chars
+jsRtBrace = stringTok "}"
+
+data JsStatement =
+  JsStmtBlock (List JsStatement)
+  | JSExpressionStatement JsExpr
+  | JsIfStmt (JsExpr, JsStatement)
+  deriving (Eq, Ord, Show)
+
+
+-- parse jsIfStmt "if(true){true;}"
+-- parse jsIfStmt "if (true) { true; }"
+-- parse jsIfStmt "if(true===true){true;}"
+jsIfStmt :: Parser (JsExpr, JsStatement)
+jsIfStmt =
+  do
+    _ <- jsIfTok
+    e1 <- betweenCharTok '(' ')' jsExpr
+    e2 <- betweenCharTok '{' '}' jsStmtBlock
+    pure (e1, JsStmtBlock e2)
+ 
+jsStmtBlock :: Parser (List JsStatement)
+jsStmtBlock =
+  do
+    s <- jsStatement
+    _ <- jsCommaTok
+    pure (s :. Nil)
+
+
+jsStatement :: Parser JsStatement
+jsStatement =
+  (
+    JSExpressionStatement <$> jsExpr
+    ||| JsIfStmt <$> jsIfStmt  
+    ||| JsStmtBlock <$> jsStmtBlock
   )
